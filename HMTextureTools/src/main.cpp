4206be31a47fd634bd2b61a7009db78dab38ae4e
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2023-04-17 05:58:40 +0100

    Bump version to v1.5.4

--- HMTextureTools/src/main.cpp
+++ HMTextureTools/src/main.cpp
@@ -8,7 +8,7 @@
 #include "Global.h"
 #include "Texture.h"
 
-argparse::ArgumentParser program("HMTextureTools", "v1.5.3");
+argparse::ArgumentParser program("HMTextureTools", "v1.5.4");
 
 void toUppercase(std::string &inputstr)
 {
