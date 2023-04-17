145287e3f4a0b83b62e830ce689552a232aae1a8
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2023-04-17 04:56:40 +0100

    Bump internal versions

--- HMLanguageTools/src/main.cpp
+++ HMLanguageTools/src/main.cpp
@@ -11,7 +11,7 @@
 #define LOG(x) std::cout << x << std::endl
 #define LOG_AND_EXIT(x) std::cout << x << std::endl; std::exit(0)
 
-argparse::ArgumentParser program("HMLanguageTools", "v1.5.1");
+argparse::ArgumentParser program("HMLanguageTools", "v1.5.2");
 
 void toUppercase(std::string &inputstr)
 {
--- HMTextureTools/src/main.cpp
+++ HMTextureTools/src/main.cpp
@@ -8,7 +8,7 @@
 #include "Global.h"
 #include "Texture.h"
 
-argparse::ArgumentParser program("HMTextureTools", "v1.5.1");
+argparse::ArgumentParser program("HMTextureTools", "v1.5.2");
 
 void toUppercase(std::string &inputstr)
 {
