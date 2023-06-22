4b17043472fa7dab55a72d98f3f111ac23cf462d
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2023-06-22 12:37:44 +0100

    chore: bump internal versions

--- Tools/HMTextureTools/src/main.cpp
+++ Tools/HMTextureTools/src/main.cpp
@@ -8,7 +8,7 @@
 #include "Global.h"
 #include "Texture.h"
 
-argparse::ArgumentParser program("HMTextureTools", "v1.6.0");
+argparse::ArgumentParser program("HMTextureTools", "v1.6.1");
 
 void toUppercase(std::string &inputstr)
 {
