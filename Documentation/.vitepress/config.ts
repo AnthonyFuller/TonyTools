f22d7429f00a7515eb744752a6c001aba1290659
AnthonyFuller <anthony@hitmods.com>
2024-05-12 15:58:22 +0100

    docs: update vitepress

--- Documentation/.vitepress/config.ts
+++ Documentation/.vitepress/config.ts
@@ -1,9 +1,4 @@
 import { defineConfig } from "vitepress"
-import { SitemapStream } from 'sitemap'
-import { createWriteStream } from 'node:fs'
-import { resolve } from 'node:path'
-
-const links: any = []
 
 export default defineConfig({
     title: "TonyTools",
@@ -64,23 +59,7 @@ export default defineConfig({
         }
     },
     cleanUrls: true,
-    // Sitemap setup
-    transformHtml: (_, id, { pageData }) => {
-        if (!/[\\/]404\.html$/.test(id))
-            links.push({
-                url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
-                changefreq: pageData.params?.changefreq ?? "weekly",
-                priority: pageData.params?.priority ?? 0.5
-            })
-    },
-    buildEnd: async ({ outDir }) => {
-        const sitemap = new SitemapStream({
-            hostname: 'https://tonytools.win'
-        })
-        const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
-        sitemap.pipe(writeStream)
-        links.forEach((link) => sitemap.write(link))
-        sitemap.end()
-        await new Promise((r) => writeStream.on('finish', r))
+    sitemap: {
+        hostname: "https://tonytools.win"
     }
 })
